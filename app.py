ef9fab47c960baab5af91cd2ceba526a82965284
andrei-pivavarchyk <Andreyatake666@mail.ru>
2025-05-09 16:09:04 +0300

    Fix port binding and use Gunicorn

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -7,4 +8,6 @@ def hello_world():
     return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Получаем порт из переменной окружения, иначе используем 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
